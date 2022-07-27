54b6b09b3365b7d8d9046540506656aa6fbc95b9
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-07-27 15:55:10 -0700

    revised about css and jsx and added basic forms directory

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,53 @@
-export default function About() {
-  return <p>I am the About page or something</p>;
-}
+import {
+  InputControl,
+  SelectControl,
+  TextAreaControl,
+  CheckboxControl,
+  FormButton,
+  Fieldset,
+} from '../Forms/FormControls.jsx';
 
+import styles from './About.css';
+
+export default function About() {
+  return (
+    <section className={styles.About}>
+      Placeholdeth
+      <form>
+        <Fieldset legend="Credentials">
+          <InputControl
+            label="email"
+            name="email"
+            placeholder="enter your email"
+            type="email"
+            required
+          />
+
+          <InputControl
+            className={styles.PasswordControl}
+            label="password"
+            name="password"
+            placeholder="choose a password"
+            type="password"
+            required
+          />
+        </Fieldset>
+
+        <SelectControl label="type">
+          <option>A</option>
+          <option>B</option>
+          <option>C</option>
+        </SelectControl>
+
+        <CheckboxControl label="Easy Mode?" text="Yes" />
+
+        <TextAreaControl
+          label="bio"
+          placeholder="tell us about yourself"
+        />
+
+        <FormButton>Submit</FormButton>
+      </form>
+    </section>
+  );
+}
