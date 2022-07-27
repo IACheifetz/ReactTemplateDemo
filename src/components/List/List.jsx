3c0489505847908cac1212cb1b7d4c8cba5c39e0
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-07-27 16:00:50 -0700

    revised home css to have variable padding, created a list css file and wrote out a more comprehensive dummy list page

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,3 +1,15 @@
+import styles from './List.css';
+
 export default function List() {
-  return <p>I am the List page, I dont know dont ask me</p>;
+  return (
+    <ul className={styles.List}>
+      {Array.from({ length: 10 }, (x, i) => i + 1).map(
+        (n) => (
+          <li key={n} className="accent-theme">
+            list item {n}
+          </li>
+        )
+      )}
+    </ul>
+  );
 }
