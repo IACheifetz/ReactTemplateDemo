cb8c1f8352157a99853476a3b975743fd3c2baae
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-07-31 21:16:14 -0700

    added jest dependencies and adjusted index html and app jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,21 @@ import {
 } from 'react-router-dom';
 import Layout from './Page/Layout.jsx';
 import Home from './Home/Home.jsx';
-import List from './List/List.jsx';
+import Pokedex from './Pokedex/Pokedex.jsx';
 import About from './About/About.jsx';
-  
+
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="list" element={<List />} />
+          <Route path="pokedex" element={<Pokedex />} />
           <Route path="about" element={<About />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route
+            path="*"
+            element={<Navigate to="/" replace />}
+          />
         </Route>
       </Routes>
     </Router>
