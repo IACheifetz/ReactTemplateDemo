672af49fe963d0992b36b9f498da9a19f5018911
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-07-31 21:22:37 -0700

    adjusted about css and jsx, adjusted form controls css and jsx and global css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,8 +3,10 @@ import {
   SelectControl,
   TextAreaControl,
   CheckboxControl,
-  FormButton,
-  Fieldset,
+  OptionGroupControl,
+  CheckboxOption,
+  RadioOption,
+  FormButtonControl,
 } from '../Forms/FormControls.jsx';
 
 import styles from './About.css';
@@ -12,26 +14,23 @@ import styles from './About.css';
 export default function About() {
   return (
     <section className={styles.About}>
-      Placeholdeth
       <form>
-        <Fieldset legend="Credentials">
-          <InputControl
-            label="email"
-            name="email"
-            placeholder="enter your email"
-            type="email"
-            required
-          />
+        <InputControl
+          label="email"
+          name="email"
+          placeholder="enter your email"
+          type="email"
+          required
+        />
 
-          <InputControl
-            className={styles.PasswordControl}
-            label="password"
-            name="password"
-            placeholder="choose a password"
-            type="password"
-            required
-          />
-        </Fieldset>
+        <InputControl
+          className={styles.PasswordControl}
+          label="password"
+          name="password"
+          type="password"
+          placeholder="choose a password"
+          required
+        />
 
         <SelectControl label="type">
           <option>A</option>
@@ -41,12 +40,36 @@ export default function About() {
 
         <CheckboxControl label="Easy Mode?" text="Yes" />
 
+        <OptionGroupControl label="Check all that apply" size="125px">
+          <CheckboxOption text="Monday" />
+          <CheckboxOption text="Tuesday" />
+          <CheckboxOption text="Thursday" />
+          <CheckboxOption text="Wednesday" />
+          <CheckboxOption text="Friday" />
+          <CheckboxOption text="Saturday" />
+          <CheckboxOption text="Sunday" />
+        </OptionGroupControl>
+
+        <OptionGroupControl
+          name="pet"
+          label="Favorite Pet"
+          size="95px"
+        >
+          <RadioOption value={1} text="dog" />
+          <RadioOption value={2} text="cat" />
+          <RadioOption value={3} text="bird" />
+          <RadioOption value={4} text="fish" />
+          <RadioOption value={5} text="lizard" />
+          <RadioOption value={6} text="snake" />
+          <RadioOption value={7} text="llama" />
+        </OptionGroupControl>
+
         <TextAreaControl
           label="bio"
           placeholder="tell us about yourself"
         />
 
-        <FormButton>Submit</FormButton>
+        <FormButtonControl>Submit</FormButtonControl>
       </form>
     </section>
   );
