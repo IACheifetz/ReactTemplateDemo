9f2836a64abf5d8e70f836f96f5b9709223bc4e9
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-08-02 15:28:55 -0700

    added user auth css and jsx in components, reworked header and slideout menu css and jsx and created blank navigation files

---
+++ src/components/UserAuth/UserAuth.jsx
@@ -0,0 +1,9 @@
+import styles from './UserAuth.css';
+
+export default function UserAuth() {
+  return (
+    <section className={styles.UserAuth}>
+      V: Under Construction :V
+    </section>
+  );
+}
