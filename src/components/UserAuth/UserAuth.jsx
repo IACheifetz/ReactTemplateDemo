f68573469f8d54a3dc124d5bb5781da22aff2bc5
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-08-07 20:19:35 -0700

    wrote out preliminary for profile css and jsx and protectroutes jsx, adjusted userauth css and jsx and app jsx

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,9 +1,17 @@
+import Auth from './Auth.jsx';
 import styles from './UserAuth.css';
+import { useStatus } from '../../state/hooks/userAuth.js';
+import Profile from './Profile.jsx';
+import { Navigate } from 'react-router-dom';
 
 export default function UserAuth() {
+  const { user, profile } = useStatus();
+
+  if (user && profile) return <Navigate to="/" />;
+
   return (
     <section className={styles.UserAuth}>
-      V: Under Construction :V
+      {user ? <Profile /> : <Auth />}
     </section>
   );
 }
